c232e0acc9bd350f2fa66c1457c7affbff051474
Steinthor <sghice@gmail.com>
2020-07-22 04:52:46 +0000

    pruning code

--- sketch_jul21a/sketch_jul21a.ino
+++ sketch_jul21a/sketch_jul21a.ino
@@ -145,24 +145,24 @@ void printValues() {
   Serial.println();
 }
 
-void drawchar(void) {
-  display.clearDisplay();
-
-  display.setTextSize(1);      // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE); // Draw white text
-  display.setCursor(0, 0);     // Start at top-left corner
-  display.cp437(true);         // Use full 256 char 'Code Page 437' font
-
-  //   Not all the characters will fit on the display. This is normal.
-  //   Library will draw what it can and the rest will be clipped.
-  for (int16_t i = 0; i < 256; i++) {
-    if (i == '\n') display.write(' ');
-    else          display.write(i);
-  }
-
-  display.display();
-  delay(2000);
-}
+//void drawchar(void) {
+//  display.clearDisplay();
+//
+//  display.setTextSize(1);      // Normal 1:1 pixel scale
+//  display.setTextColor(SSD1306_WHITE); // Draw white text
+//  display.setCursor(0, 0);     // Start at top-left corner
+//  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+//
+//  //   Not all the characters will fit on the display. This is normal.
+//  //   Library will draw what it can and the rest will be clipped.
+//  for (int16_t i = 0; i < 256; i++) {
+//    if (i == '\n') display.write(' ');
+//    else          display.write(i);
+//  }
+//
+//  display.display();
+//  delay(2000);
+//}
 
 
 
